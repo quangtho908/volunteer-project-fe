11cbc2c6e629b278f4bf5a59e414fccbff5f270c
phamthuytienn <20130432@st.hcmuaf.edu.vn>
2024-04-12 15:02:35 +0700

    xem danh sach cac chien dich, duyet du an phia truong

--- src/components/ListCampain/ListCampaign.jsx
+++ src/components/ListCampain/ListCampaign.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
 import SubHeader from '../Shared/SubHeader'
 import Footer from '../Shared/Footer/Footer'
 import Header from '../Shared/Header/Header'
-import img from '../../images/features/baby.png'
 import { useNavigate } from 'react-router-dom'
-import doctorBg from '../../images/img/doctors-bg.jpg';
 import { Link } from 'react-router-dom';
 import { FaLocationArrow, FaRegThumbsUp, FaDollarSign, FaComment } from "react-icons/fa";
-import { FaClock } from "react-icons/fa";
+import axios from 'axios';
+ import React, { useEffect, useState } from 'react';
 
 const ListCampaign = () => {
-    const weArePleaseStyle = {
-        backgroundColor: "antiquewhite",
-        height: "60vh",
-        background: `url(${doctorBg}) no-repeat`,
-        backgroundPosition: 'center center',
-        backgroundSize: 'cover',
-        padding: "10px",
-        position: "relative",
-        marginTop: 200,
-        marginBottom: 100
-    }
+    const [campaigns, setCampaigns] = useState([]);
     const navigate = useNavigate();
 
-    const handleServiceClick = (campaignId) => {
+    useEffect(() => {
+        const getAllCampaigns = async () => {
+            try {
+                const response = await fetch('https://project-software-z6dy.onrender.com/strategies', {
+                    method: 'GET',
+                    headers: {
+                        'accept': '*/*',
+                        'Content-Type': 'application/json',
+                        'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJxdWFuZ3RobzIzMDYyMDAyQGdtYWlsLmNvbSIsInJvbGUiOjAsInRpbWUiOjE3MTI4ODc4ODE1MTEsImlhdCI6MTcxMjg4Nzg4MX0.LC8YPbX1i_Zi4HSMoZ3pgpoq5iA8RtgxF9B8_lIEKnI'
+                    }
+                });
+                const data = await response.json();
+                setCampaigns(data.data);
+            } catch (error) {
+                console.error('Error fetching campaigns:', error);
+            }
+        };
+
+        getAllCampaigns();
+
+        return () => {
+            // Cleanup logic (if needed)
+        };
+    }, []);
+
+    const handleCampaignClick = (campaignId) => {
         navigate(`/campaigns/${campaignId}`);
     };
     return (
@@ -33,78 +46,47 @@ const ListCampaign = () => {
 
             <div className="container">
                 <div className="row justify-content-center">
-
-                    {
-                        Array(6).fill(null).map((_item, id) => (
-                    <div className='col-10 align-selft-center  text-center mt-5'  key={id + 6} onClick={() => handleServiceClick(id + 6)}>
-                        {/* <Pagination
-                                    showSizeChanger
-                                    onShowSizeChange={onShowSizeChange}
-                                    total={meta?.total}
-                                    pageSize={size}
-                                /> */}
-                        <div className="mb-4 rounded" style={{ background: '#f3f3f3' }}>
-                            <div className='d-flex p-3 justify-content-between'>
-                                <div className='d-flex gap-3'>
-                                    <div className='doc-img-fluid d-flex align-items-center'>
-                                        {/* { data?.img && <img src={data?.img} className="" alt="User Image" />} */}
-                                        <img
-                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbFl4LFohrLy-RNdC7vp_c8M6PR0FFm55OxxjlmsIxow&s"
-                                            className="" alt="User Image" />
+                    {campaigns.map(campaign => (
+                        <div key={campaign.id} className='col-10 align-selft-center text-center mt-5'>
+                            <div className="mb-4 rounded" style={{ background: '#f3f3f3' }}>
+                                <div className='d-flex p-3 justify-content-between'>
+                                    <div className='d-flex gap-3'>
+                                        <div className='doc-img-fluid d-flex align-items-center'>
+                                            <img
+                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbFl4LFohrLy-RNdC7vp_c8M6PR0FFm55OxxjlmsIxow&s"
+                                                alt="User Image" />
+                                        </div>
+                                        <div className="doc-info  d-flex flex-column align-items-center justify-content-center">
+                                            <h5 className='mb-0'><Link to={`/campaigns/${campaign.id}`}>{campaign.name}</Link></h5>
+                                            <div className="clinic-details mt-2">
+                                                <p className="form-text text-secondary"><FaLocationArrow /> {campaign.place}</p>
+                                                <p className="form-text text-secondary">Duyệt trước: {new Date(campaign.endAt).toLocaleString()}</p>
+                                                <span className="tag bg-secondary text-white p-1 rounded">Đã duyệt</span>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="doc-info  d-flex flex-column align-items-center justify-content-center">
-                                        <h5 className='mb-0'><Link to={`/detail/studentList/1`}>Mùa Hè Xanh:
-                                            Hành động cho môi trường</Link></h5>
-
-                                        {/* <p className="doc-department m-0"><img src={showImg} className="img-fluid" alt="Speciality" />Urology</p> */}
-                                        <div className="clinic-details mt-2">
-                                            <p className="form-text text-secondary"><FaLocationArrow /> Ba Tri,
-                                                Bến Tre</p>
-                                            <p className="form-text text-secondary">Duyệt trước:
-                                                16/06/2023</p>
-                                            <span className="tag bg-secondary text-white p-1 rounded">Đã duyệt</span>
-                                            {/* <ul className="clinic-gallery mt-3">
-                                <li>
-                                    <FcLike />
-                                </li>
-                                <li>
-                                    <FcLike />
-                                </li>
-                                <li>
-                                    <FcLike />
-                                </li>
-                                <li>
-                                    <FcLike />
-                                </li>
-                            </ul> */}
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <div className="doc-info-right me-3">
-                                    <div className="clinic-booking d-flex flex-column justify-content-center align-items-center h-100">
-                                        <div className='clinic-booking-button mb-2 w-100'>
-                                            <Link to={'/campaigns/'+id+6} className="btn btn-primary btn-block">Chi tiết</Link>
-                                        </div>
-                                        <div className='clinic-booking-button mb-2 w-100 mt-10 mb-10'>
-                                            <Link to={``} className="btn btn-success btn-block">Duyệt</Link>
-                                        </div>
-                                        <div className='clinic-booking-button w-100'>
-                                            <Link to={``} className="btn btn-danger btn-block">Từ chối</Link>
+                                    <div className="doc-info-right me-3">
+                                        <div className="clinic-booking d-flex flex-column justify-content-center align-items-center h-100">
+                                            <div className='clinic-booking-button mb-2 w-100'>
+                                                <Link to={`/campaigns/${campaign.id}`} className="btn btn-primary btn-block">Chi tiết</Link>
+                                            </div>
+                                            <div className='clinic-booking-button mb-2 w-100 mt-10 mb-10'>
+                                                <Link to={`/booking/${campaign.id}`} className="btn btn-success btn-block">Duyệt</Link>
+                                            </div>
+                                            <div className='clinic-booking-button w-100'>
+                                                <Link to={`/booking/${campaign.id}`} className="btn btn-danger btn-block">Từ chối</Link>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                            ))}
+                    ))}
                 </div>
             </div>
-
-
             <Footer />
         </>
     )
 }
 
-export default ListCampaign
+export default ListCampaign;
