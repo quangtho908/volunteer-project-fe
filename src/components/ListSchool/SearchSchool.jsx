d47a5bb8af30d3f2af697eb71c1c1d0d134964f1
TRUONGPHANNGOCTRUC <20130449@st.hcmuaf.edu.vn>
2024-04-13 09:18:59 +0700

    them logo

--- src/components/ListSchool/SearchSchool.jsx
+++ src/components/ListSchool/SearchSchool.jsx
@@ -30,33 +30,11 @@ const SearchSchool = () => {
         navigate('/listProjectSV/' + id);
     }
 
-    const mockSchoolsData = [
-        { id: 1, name: "Universities 1", specialty: "Specialty 1", gender: "Male" },
-        { id: 2, name: "Universities 2", specialty: "Specialty 2", gender: "Female" },
-        { id: 3, name: "Universities 3", specialty: "Specialty 1", gender: "Male" },
-        { id: 4, name: "Universities 4", specialty: "Specialty 2", gender: "Female" },
-        { id: 5, name: "Universities 5", specialty: "Specialty 1", gender: "Male" },
-        { id: 6, name: "Universities 6", specialty: "Specialty 2", gender: "Female" },
-        { id: 7, name: "Universities 7", specialty: "Specialty 1", gender: "Male" },
-        { id: 8, name: "Universities 8", specialty: "Specialty 2", gender: "Female" },
-        { id: 9, name: "Universities 9", specialty: "Specialty 1", gender: "Male" },
-        { id: 10, name: "Universities 10", specialty: "Specialty 2", gender: "Female" },
-        { id: 11, name: "Universities 11", specialty: "Specialty 1", gender: "Male" },
-        { id: 12, name: "Universities 12", specialty: "Specialty 2", gender: "Female" },
-        { id: 13, name: "Universities 13", specialty: "Specialty 1", gender: "Male" },
-        { id: 14, name: "Universities 14", specialty: "Specialty 2", gender: "Female" },
-        { id: 15, name: "Universities 15", specialty: "Specialty 1", gender: "Male" },
-        { id: 16, name: "Universities 16", specialty: "Specialty 2", gender: "Female" },
-    ];
     const [univercity, setUniversity] = useState([]);
     // Mock meta data
     const mockMeta = { total: univercity.length };
-    //  // Render danh sách trường đại học
-    //  const startIndex = (page - 1) * size;
-    //  const endIndex = startIndex + size;
-    //  const displayedSchools = mockSchoolsData.slice(startIndex, endIndex);
-    
-    const handleGetListUniversities = async (email, password) => {
+    const imageUrl = "https://upload.wikimedia.org/wikipedia/vi/e/e1/Logo_HCMUAF.svg";
+    const handleGetListUniversities = async () => {
         try {
             const response = await fetch('https://project-software-z6dy.onrender.com/universities', {
                 method: 'GET'
@@ -95,7 +73,7 @@ const SearchSchool = () => {
             }} className='d-flex p-3 justify-content-between'>
                 <div className='d-flex gap-3'>
                     <div className='doc-img-fluid d-flex align-items-center'>
-                        <img src={item.avatar} alt={item.name} className="" style={{ width: 50, height: 50, marginRight: 10 }} />
+                        <img src={imageUrl} alt={item.name} className="" style={{ width: 80, height: 80, marginRight: 10}} />
                     </div>
                     <div className="doc-info">
                         <h5 className='mb-0'>{item.name}</h5>
@@ -132,11 +110,7 @@ const SearchSchool = () => {
                                     onShowSizeChange={onShowSizeChange}
                                     total={univercity.length}
                                     pageSize={size}
-                                    // current={page}
-                                    // pageSize={10}
-                                    // total={mockMeta.total}
-                                    // showSizeChanger
-                                    // onShowSizeChange={onShowSizeChange}
+                                    
                                 />
                             </div>
                         </div>
