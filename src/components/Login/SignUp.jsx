a862455da18167c29c158b924778a3a0c6412394
YourUsername <your@email.com>
2024-07-10 00:15:46 +0700

    sign up

--- src/components/Login/SignUp.jsx
+++ src/components/Login/SignUp.jsx
@@ -1,211 +1,150 @@
-import React, { useEffect, useState } from 'react';
-import { FaCheck, FaEnvelope, FaLock, FaTimes, FaUser } from 'react-icons/fa';
-import SocialSignUp from './SocialSignUp';
-import Spinner from 'react-bootstrap/Spinner'
-import swal from 'sweetalert';
-import { useDoctorSignUpMutation, usePatientSignUpMutation } from '../../redux/api/authApi';
+import React, { useState, useEffect } from 'react';
+import { FaEnvelope, FaLock, FaUser, FaSchool } from 'react-icons/fa';
+import { useForm } from "react-hook-form";
+import Spinner from 'react-bootstrap/Spinner';
+import { useNavigate } from 'react-router-dom';
 import { message } from 'antd';
+import './SignInForm.css'; // Import the CSS file
 
-// password regex
-// ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$
-// At least one upper case English letter, (?=.*?[A-Z])
-// At least one lower case English letter, (?=.*?[a-z])
-// At least one digit, (?=.*?[0-9])
-// At least one special character, (?=.*?[#?!@$%^&*-])
-// Minimum eight in length .{8,} (with the anchors)
-
-const SignUp = ({ setSignUp }) => {
-    const [error, setError] = useState({});
+const SignUp = () => {
     const [infoError, setInfoError] = useState('');
-    const [loading, setLoading] = useState(false);
-    const formField = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-    }
-    const [user, setUser] = useState(formField)
-    const [userType, setUserType] = useState('patient');
-    const [doctorSignUp, { data: dData, isSuccess: dIsSuccess, isError: dIsError, error: dError, isLoading: dIsLoading }] = useDoctorSignUpMutation();
-    const [patientSignUp, { data: pData, isSuccess: pIsSuccess, isError: pIsError, error: pError, isLoading: pIsLoading }] = usePatientSignUpMutation();
-    const [passwordValidation, setPasswordValidation] = useState({
-        carLength: false,
-        specailChar: false,
-        upperLowerCase: false,
-        numeric: false
-    })
+    const [isLoading, setIsLoading] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+    const [fullName, setFullName] = useState('');
+    const [universityId, setUniversityId] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [universities, setUniversities] = useState([]);
 
-    const handleSignUpSuccess = () => {
-        setLoading(false);
-        setUser(formField)
-    }
     useEffect(() => {
-        // doctor account
-        if (dIsError && dError) {
-            message.error("Email Already Exist !!")
-            setLoading(false);
-        }
+        handleGetListUniversities();
+    }, []); 
 
-        if (!dIsError && dIsSuccess) {
-            handleSignUpSuccess();
-            setLoading(false);
-            setLoading(false);
-            swal({
-                icon: 'success',
-                text: `Successfully Account Created Please Verify Your email`,
-                timer: 5000
-            })
-        }
+    const handleGetListUniversities = async () => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_PUBLIC_API}/universities`, {
+                method: 'GET'
+            });
 
-        // Patient account
-        if (pIsError && pError) {
-            message.error("Email Already Exist !!")
-            setLoading(false);
+            const data = await response.json();
+        
+            if (response.ok) {
+                setUniversities(data.data);
+            } else {
+                console.error(data?.message);
+            }
+        } catch (error) {
+            console.error(error);
         }
-        if (!pIsError && pIsSuccess) {
-            handleSignUpSuccess();
-            setLoading(false);
-            setSignUp(false);
-            swal({
-                icon: 'success',
-                text: `Successfully ${userType === 'doctor' ? 'Doctor' : 'Patient'} Account Created Please Login`,
-                timer: 2000
-            })
-        }
+    };
 
-    }, [dIsError, dError, pError, pIsError, , pIsLoading, dIsLoading, pData, dData, setSignUp, setLoading, dIsSuccess])
-
-    const [emailError, setEmailError] = useState({
-        emailError: false
-    })
-
-    const handleEmailError = (name, value) => {
-        if (name === 'email') {
-            setEmailError({
-                emailError: /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
-            })
+    const onSubmit = async (data) => {
+        setIsLoading(true);
+        try {
+            const response = await fetch(`${process.env.REACT_APP_PUBLIC_API}/auth/signup`, {
+                method: 'POST',
+                headers: {
+                    'accept': '*/*',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    fullName: data.fullName,
+                    universityId: data.universityId,
+                    email: data.email,
+                    password: data.password,
+                }),
+            });
+    
+            const result = await response.json();
+    
+            if (response.ok) {
+                localStorage.setItem("token", JSON.stringify(result.data.token));
+                const userRole = result?.data?.role; // Assuming role is directly accessible in response
+                localStorage.setItem("role", JSON.stringify(userRole));
+    
+                message.success('Successfully signed up');
+                navigate('/');
+            } else {
+                if (result?.statusCode === 400 && result?.message === 'User is already') {
+                    setInfoError('Email đã tồn tại trong hệ thống');
+                } else {
+                    console.error(result?.message);
+                    setInfoError('Email đã tồn tại trong hệ thống');
+                }
+            }
+        } catch (error) {
+            console.error(error);
+            setInfoError('Failed to sign up');
+        } finally {
+            setIsLoading(false);
         }
-    }
-    const hanldeValidation = (name, value) => {
-        if (name === 'password') {
-            setPasswordValidation({
-                carLength: (value.length > 8),
-                specailChar: /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/.test(value),
-                upperLowerCase: /^(?=.*[a-z])(?=.*[A-Z])/.test(value),
-                numeric: /^(?=.*\d)/.test(value),
-            })
-        }
-    }
-
-    const hanldeOnChange = (e) => {
-        let { name, value } = e.target;
-        hanldeValidation(name, value)
-        handleEmailError(name, value)
-        let isPassValid = true;
-
-        if (value === 'email') {
-            isPassValid = /\S+@\S+\.\S+/.test(value);
-        }
-        if (value === 'password') {
-            isPassValid = ((value.length > 8)
-                && /[ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/.test(value)
-                && /^(?=.*[a-z])(?=.*[A-Z])/.test(value)
-                && /^(?=.*\d)/.test(value))
-        }
-        if (isPassValid) {
-            const newPass = { ...user };
-            newPass[name] = value
-            setUser(newPass)
-        }
-    }
-
-    const handleUserTypeChange = (e) => {
-        setUserType(e.target.value);
-    }
-    const hanldeOnSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        if (userType === "doctor") {
-            doctorSignUp(user);
-        } else {
-            patientSignUp(user)
-        }
-    }
+    };
+    
+    const handleUniversityChange = (e) => {
+        const selectedUniversityId = e.target.value;
+        setUniversityId(selectedUniversityId);
+        console.log("Selected University ID:", selectedUniversityId);
+    };
 
     return (
-        <form className="sign-up-form" onSubmit={hanldeOnSubmit}>
-            <h2 className="title">Sign Up</h2>
-            <div className="input-field">
-                <span className="fIcon"><FaUser /></span>
-                <input placeholder="First Name" name="firstName" type="text" onChange={(e) => hanldeOnChange(e)} value={user.firstName} />
-            </div>
-            <div className="input-field">
-                <span className="fIcon"><FaUser /></span>
-                <input placeholder="Last Name" name="lastName" type="text" onChange={(e) => hanldeOnChange(e)} value={user.lastName} />
-            </div>
-            <div className="input-field">
-                <span className="fIcon"><FaEnvelope /></span>
-                <input placeholder="Email" name="email" type="email" onChange={(e) => hanldeOnChange(e)} value={user.email} />
-            </div>
-            <div className="input-field">
-                <span className="fIcon"><FaLock /></span>
-                <input type="password" name="password" placeholder="password" onChange={(e) => hanldeOnChange(e)} value={user.password} />
-            </div>
-            <div className='input-field d-flex align-items-center gap-2 justify-content-center'>
-                <div className='text-nowrap'>I'M A</div>
-                <select
-                    className="form-select w-50"
-                    aria-label="select"
-                    onChange={(e) => handleUserTypeChange(e)}
-                    defaultValue='patient'
-                >
-                    <option value="patient">Patient</option>
-                    <option value="doctor">Doctor</option>
-                </select>
-            </div>
-            {error.length && <h6 className="text-danger text-center">{error}</h6>}
-            {infoError && <h6 className="text-danger text-center">{infoError}</h6>}
-            <button type="submit"
-                className="btn btn-primary btn-block mt-2 iBtn"
-                disabled={
-                    passwordValidation.carLength && passwordValidation.numeric && passwordValidation.upperLowerCase && passwordValidation.specailChar && emailError.emailError ? "" : true
-                }
-            >
-                {loading ? <Spinner animation="border" variant="info" /> : "Sign Up"}
-            </button>
-
-            <div className="password-validatity mx-auto">
-
-                <div style={emailError.emailError ? { color: "green" } : { color: "red" }}>
-                    <p>{passwordValidation.numeric ? <FaCheck /> : <FaTimes />}
-                        <span className="ms-2">Must Have Valid Email.</span></p>
+        <>
+            <form className="sign-in-form" onSubmit={handleSubmit(onSubmit)}>
+                <h2 className="title">Đăng ký</h2>
+                <div className="input-field">
+                    <span className="fIcon"><FaUser /></span>
+                    <input 
+                        onChange={(e) => setFullName(e.target.value)} 
+                        placeholder="Enter Your Name" 
+                        type="text" 
+                        {...register("fullName", { required: true })}
+                    />
                 </div>
-
-                <div style={passwordValidation.carLength ? { color: "green" } : { color: "red" }}>
-                    <p>{passwordValidation.numeric ? <FaCheck /> : <FaTimes />}
-                        <span className="ms-2">Password Must Have atlast 8 character.</span></p>
+                {errors.fullName && <span className="text-danger">This field is required</span>}
+                <div className="input-field">
+                    <span className="fIcon"><FaSchool /></span>
+                    <select 
+                        onChange={handleUniversityChange} 
+                        {...register("universityId", { required: true })}
+                        className={`form-select ${errors.universityId ? 'is-invalid' : ''}`}
+                    >
+                        <option value="">Chọn trường</option>
+                        {universities.map((university) => (
+                            <option key={university.id} value={university.id}>{university.name}</option>
+                        ))}
+                    </select>
                 </div>
-
-                <div style={passwordValidation.specailChar ? { color: "green" } : { color: "red" }}>
-                    <p>{passwordValidation.numeric ? <FaCheck /> : <FaTimes />}
-                        <span className="ms-2">Password Must Have a special cracter.</span></p>
+                {errors.universityId && <span className="text-danger">This field is required</span>}
+                <div className="input-field">
+                    <span className="fIcon"><FaEnvelope /></span>
+                    <input 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        placeholder="Enter Your Email" 
+                        type="email" 
+                        {...register("email", { required: true })}
+                    />
                 </div>
-
-                <div style={passwordValidation.upperLowerCase ? { color: "green" } : { color: "red" }}>
-                    <p>{passwordValidation.numeric ? <FaCheck /> : <FaTimes />}
-                        <span className="ms-2">Password Must Have uppercase and lower case.</span></p>
+                {errors.email && <span className="text-danger">This field is required</span>}
+                <div className="input-field">
+                    <span className="fIcon"><FaLock /></span>
+                    <input 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        type="password" 
+                        placeholder="Enter Your Password" 
+                        {...register("password", { required: true })}
+                    />
                 </div>
-
-                <div style={passwordValidation.numeric ? { color: "green" } : { color: "red" }}>
-                    <p>{passwordValidation.numeric ? <FaCheck /> : <FaTimes />}
-                        <span className="ms-2">Password Must Have Number.</span></p>
-                </div>
+                {errors.password && <span className="text-danger">This field is required</span>}
+                {infoError && <p className="text-danger">{infoError}</p>}
+                <button className="iBtn" type="submit" disabled={isLoading}>
+                    {isLoading ? <Spinner animation="border" variant="info" /> : "Đăng ký"}
+                </button>
+            </form>
+            <div className="form-footer">
+                <p>Đã có tài khoản</p>
+                <a href="/login/student">Đăng nhập</a>
             </div>
-
-            <p className="social-text">Or Sign up with social account</p>
-            <SocialSignUp />
-        </form>
-
+        </>
     );
 };
 
