6c90e7f387a43301148af5834a81aae5119ec90d
YourUsername <your@email.com>
2024-07-10 01:14:56 +0700

    edit frontend ListProject

--- src/components/ListProject/ListProject.js
+++ src/components/ListProject/ListProject.js
@@ -108,30 +108,32 @@ const ListProject = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12 col-lg-4 col-xl-3">
-              <div className="p-3 rounded" style={{ background: '#f3f3f3' }}>
-                <div className="mb-3">
+              <div className="p-3 rounded bg-gray-200">
+                <div className="d-flex justify-content-between">
                   <button onClick={() => window.location.href = '/manageSchools'}
-                    style={{ marginLeft: '50px' }}
+                    style={{height: '40px' }}
                     className="btn btn-primary mt-3"> Quản lý Trường</button>
                   <button onClick={() => window.location.href = '/createCampaigns'}
-                    style={{ marginLeft: '50px' }}
+                    style={{height: '40px' }}
                     className="btn btn-primary mt-3"> Tạo chiến dịch</button>
                 </div>
-                <div className="md-3" >
-                  <h6 style={{ color: '#05335c' }}>Mùa hè xanh</h6>
+              </div>
+              <div className="p-3 rounded bg-gray-200">
+                <div className="md-3">
+                  <h6 className="text-blue-700">Mùa hè xanh</h6>
                   <div className="card shadow border-0 mb-5">
-                    <img src="https://dainam.edu.vn/uploads/images/ckeqouwcp4qi5juiulff20230802101300_thump.jpg" alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
+                    <img src="https://dainam.edu.vn/uploads/images/ckeqouwcp4qi5juiulff20230802101300_thump.jpg" alt="" className="img-fluid rounded-t-lg" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
                     <div className="p-2">
                       <p className="mb-4">Mùa hè xanh 2023: Chiến dịch tình nguyện đầy nhiệt huyết của tuổi trẻ DNU tại mảnh đất Cao Bằng</p>
-                      <a href='https://dainam.edu.vn/vi/tin-tuc/mua-he-xanh-2023-chien-dich-tinh-nguyen-day-nhiet-huyet-cua-tuoi-tre-dnu-tai-manh-dat-cao-bang' className="apt-btn" target="_blank">Truy cập</a>
+                      <a href='https://dainam.edu.vn/vi/tin-tuc/mua-he-xanh-2023-chien-dich-tinh-nguyen-day-nhiet-huyet-cua-tuoi-tre-dnu-tai-manh-dat-cao-bang' className="btn btn-primary" target="_blank">Truy cập</a>
                     </div>
                   </div>
 
                   <div className="card shadow border-0 mb-5">
-                    <img src="https://doanthanhnien.vn/Content/uploads/images/133382190131936031_356622827_670872445078522_7208570064784879038_n.jpg" alt="" className="img-fluid" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
+                    <img src="https://doanthanhnien.vn/Content/uploads/images/133382190131936031_356622827_670872445078522_7208570064784879038_n.jpg" alt="" className="img-fluid rounded-t-lg" style={{ maxHeight: '17rem', objectFit: 'cover' }} />
                     <div className="p-2">
                       <p className="mb-4">Mùa hè xanh - Nơi cống hiến tri thức trẻ, góp phần dựng xây quê hương, đất nước.</p>
-                      <a href='https://doanthanhnien.vn/tin-tuc/mua-he-xanh/mua-he-xanh---noi-cong-hien-tri-thuc-tre-gop-phan-dung-xay-que-huong-dat-nuoc' className="apt-btn" target="_blank">Truy cập</a>
+                      <a href='https://doanthanhnien.vn/tin-tuc/mua-he-xanh/mua-he-xanh---noi-cong-hien-tri-thuc-tre-gop-phan-dung-xay-que-huong-dat-nuoc' className="btn btn-primary" target="_blank">Truy cập</a>
                     </div>
                   </div>
                 </div>
@@ -139,7 +141,7 @@ const ListProject = () => {
             </div>
 
             <div className="col-md-12 col-lg-8 col-xl-9">
-              <div className="mb-3">
+              <div className="mb-3" style={{marginTop: 32}}>
                 <Select
                   style={{ width: 200 }}
                   placeholder="Lọc danh sách chiến dịch"
