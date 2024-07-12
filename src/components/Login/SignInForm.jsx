ff8107ba48df2c0efeea9cfbe97c35db7ce1e801
YourUsername <your@email.com>
2024-07-12 22:55:53 +0700

    edit compaignDetail

--- src/components/Login/SignInForm.jsx
+++ src/components/Login/SignInForm.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import log from '../../images/doc/info.svg';
 import register from '../../images/doc/register.svg';
 import SignIn from './SignIn';
@@ -8,6 +8,8 @@ import './SignInForm.css';
 
 const SignInForm = () => {
     const [isSignUp, setSignUp] = useState(false);
+    const { type } = useParams(); // Lấy type từ URL
+
     return (
         <div className={`${isSignUp ? "signin-signup-container sign-up-mode" : "signin-signup-container"}`}>
             <Link to="/">
@@ -15,28 +17,24 @@ const SignInForm = () => {
             </Link>
             <div className="forms-container">
                 <div className="signIn-singUp">
-                    <SignIn />
-                    {/* <SignUp setSignUp={setSignUp} /> */}
+                    <SignIn type={type} />
                 </div>
             </div>
             <div className="panels-container">
                 <div className="panel left-panel">
                     <div className="content">
-                        {/* <h3 className='text-white'>New here ?</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi beatae quas magnam!</p>
-                        <button className="iBtn transparent" onClick={() => setSignUp(true)}>Sign Up</button> */}
+                        {/* Các nội dung khác nếu cần */}
                     </div>
-                    <img src={`${log}`} alt="" className="pImg" />
+                    <img src={log} alt="info" className="pImg" />
                 </div>
-                {/* 
-                <div className="panel right-panel">
-                    <div className="content">
-                        <h3 className='text-white'>One of us ?</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi beatae quas magnam!</p>
-                        <button className="iBtn transparent" onClick={() => setSignUp(false)}>Sign In</button>
-                    </div>
-                    <img src={`${register}`} alt="" className="pImg" />
-                </div> */}
+                {/* Các panel khác nếu cần */}
+            </div>
+            <div className="marquee-container">
+                <div className="marquee-content">
+                    {type === 'admin' && <p className="marquee-text">Chào mừng Lãnh đạo địa phương, xin vui lòng đăng nhập để bắt đầu hành trình quản lý chiến dịch tình nguyện.</p>}
+                    {type === 'school' && <p className="marquee-text">Kính chào Nhà trường, xin vui lòng đăng nhập để điều phối và quản lý các chiến dịch tình nguyện!</p>}
+                    {type === 'student' && <p className="marquee-text">Chào mừng các bạn Sinh viên, xin vui lòng đăng nhập để tham gia và trải nghiệm những chiến dịch tình nguyện đầy thú vị.</p>}
+                </div>
             </div>
         </div>
     );
